8860144f35d2ee8210e26f61bc1762c3f49d4888
DanielJanowicz <102616987+DanielJanowicz@users.noreply.github.com>
2022-08-28 13:07:09 -0400

    Import of packages
    Installed python packages on local computer and tested to make sure it ran correctly

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-## Section 1 
+## Importing on python packages
+import pandas as pd
+import json
+import xlrd
+import bs4
+import requests
+import sqlalchemy
+import pydub
+from google.cloud import bigquery
+
+## Section 1 - Converting excel file into dataframe
+
+
